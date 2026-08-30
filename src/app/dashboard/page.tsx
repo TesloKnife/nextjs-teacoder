8f38fa5a0179242594035395ae8c8c7e5686d627
teslo <teslo.knife@gmail.com>
2026-08-30 14:14:36 +0500

    Базовая разметка. Layout, template, loading

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default async function DashboardPage() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return (
+    <div className="p-4 bg-emerald-50 border-2 border-emerald-500 rounded-md">
+      <h1 className="text-xl  font-bold text-black">
+        Контент страницы Dashboard
+      </h1>
+    </div>
+  );
+}
