43edfb980c9d14d7da7a1a7f3594db51b894bdbe
teslo <teslo.knife@gmail.com>
2026-09-04 14:38:57 +0500

    Кеширование в appRouter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+async function getGithubProfile() {
+  const res = await fetch("https://api.github.com/orgs/vercel", {
+    cache: "force-cache",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch github profile");
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getGithubProfile();
+
   return (
-    <main className="p-10 bg-black text-white min-h-screen">Home Page</main>
+    <main className="p-10 bg-black text-white min-h-screen">
+      <h2>Профиль: {data.name}</h2>
+      <p>Desc: {data.description}</p>
+      <p>Public Repos: {data.public_repos}</p>
+    </main>
   );
 }
