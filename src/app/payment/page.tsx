116e92fc02a1f19db2630e4e83adf2ca00b80d10
teslo <teslo.knife@gmail.com>
2026-09-16 16:23:25 +0500

    Управление переменными окружения

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+export default function PaymentPage() {
+  console.log("SUPABASE URL: ", process.env.NEXT_PUBLIC_SUPABASE_URL);
+  console.log("STRIPE SECRET KEY: ", process.env.STRIPE_SECRET_KEY);
+
+  return <div>Payment Form</div>;
+}
