0aa0e2839534f4ae73996cc379d9c60a755805a6
teslo <teslo.knife@gmail.com>
2026-09-16 12:26:09 +0500

    связи в drizzle

---
+++ drizzle/0001_faithful_captain_flint.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "brands" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "products" ADD COLUMN "brand_id" uuid;--> statement-breakpoint
+ALTER TABLE "products" ADD CONSTRAINT "products_brand_id_brands_id_fk" FOREIGN KEY ("brand_id") REFERENCES "public"."brands"("id") ON DELETE no action ON UPDATE no action;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
+import { relations } from "drizzle-orm";
 import {
   integer,
   pgTable,
-  serial,
   timestamp,
   uuid,
   varchar,
@@ -12,5 +12,23 @@ export const products = pgTable("products", {
   name: varchar("name", { length: 255 }).notNull(),
   price: integer("price").notNull(),
   stock: integer("stock").notNull(),
+  brandId: uuid("brand_id").references(() => brands.id),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
+
+export const productsRelations = relations(products, ({ one }) => ({
+  brand: one(brands, {
+    fields: [products.brandId],
+    references: [brands.id],
+  }),
+}));
+
+export const brands = pgTable("brands", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 100 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const brandsRelations = relations(brands, ({ many }) => ({
+  products: many(products),
+}));
