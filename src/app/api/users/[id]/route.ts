d5a4b0f8976e219d02b5c79c12cae963b077355c
teslo <teslo.knife@gmail.com>
2026-09-16 10:31:08 +0500

    RouteHandlers

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> },
+) {
+  const { id } = await context.params;
+
+  const { searchParams } = new URL(request.url);
+  const fields = searchParams.get("fields");
+
+  return NextResponse.json(
+    {
+      message: "Success",
+      targetId: id,
+      fields,
+    },
+    {
+      status: 200,
+      headers: { "X-Cutom-Header": "random" },
+    },
+  );
+}
--- src/lib/safe-action.ts
+++
@@ -1,9 +0,0 @@
-import { createSafeActionClient } from "next-safe-action";
-
-export const actionClient = createSafeActionClient({
-  handleServerError: (error) => {
-    console.log("Server error:", error.message);
-
-    return error.message || "Internal server error";
-  },
-});
