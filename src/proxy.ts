118b61b0af06547af8e69cc096f004851708e6ee
teslo <teslo.knife@gmail.com>
2026-09-17 16:53:47 +0500

    CSP - content security policy

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,27 +2,25 @@ import { type NextRequest, NextResponse } from "next/server";
 
 // Можем получать куки пользователя и на какой url он зашел
 export default function proxy(request: NextRequest) {
-  const url = request.nextUrl;
+  const nonce = btoa(crypto.randomUUID());
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic'${process.env.NODE_ENV === "development" ? " 'unsafe-eval'" : ""};
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+`
+    .replace(/\s{2,}/g, " ")
+    .trim();
 
   const requestHeaders = new Headers(request.headers);
 
-  requestHeaders.set("x-pathname", url.pathname);
-
-  if (url.pathname.startsWith("/dashboard")) {
-    const targetUrl = new URL("/auth/login", request.url);
-
-    return NextResponse.redirect(targetUrl);
-  }
-
-  if (url.pathname === "/store/legacy") {
-    const targetUrl = new URL("/store/modern", request.url);
-
-    return NextResponse.rewrite(targetUrl, {
-      request: {
-        headers: requestHeaders,
-      },
-    });
-  }
+  requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set("Content-Security-Policy", cspHeader);
 
   const response = NextResponse.next({
     request: {
@@ -30,7 +28,7 @@ export default function proxy(request: NextRequest) {
     },
   });
 
-  response.headers.set("x-proxy-handled", "true");
+  response.headers.set("Content-Security-Policy", cspHeader);
 
   return response;
 }
